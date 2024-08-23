9ca1e0f28ed8d95ac6d13225c9aa7a6651c9766d
Jakob <j.respondek@qvest-digital.com>
2024-08-23 10:35:14 +0200

    upgrade dependencies with breaking changes

--- flutter_frontend/lib/core/app_router.dart
+++ flutter_frontend/lib/core/app_router.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter_frontend/core/app_router.gr.dart';
 
 @AutoRouterConfig()
-class AppRouter extends $AppRouter {
+class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
         AutoRoute(page: TaskOverviewRoute.page, path: '/'),
